97337f7e09d83145d1f3835209398ef47babdbe2
emmaricia40 <124712414+emmaricia40@users.noreply.github.com>
2023-02-10 18:58:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='SpeakWriteGo',
+    name="SpeakWriteGo",
     version='0.1',
-    description='A Library for transcribing audio to text',
-    author='Emmaricia',
-    url='https://github.com/emmaricia40/SpeakWriteGo',
-    packages=find_packages(),
+    description="A Library for transcribing audio to text",
+    author="Emmaricia",
+    url="https://github.com/openai/whisper",
+    license="MIT",
+    packages=["SpeakWriteGo"],
     install_requires=[
         'numpy',
         'torch',
-        'streamlit',
         'openai',
-        'openai-whisper'
     ],
 )
