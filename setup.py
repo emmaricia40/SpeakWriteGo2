09a40ca747c6d373a9b9cff9eba7659cdf28a305
emmaricia40 <124712414+emmaricia40@users.noreply.github.com>
2023-02-10 16:53:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="SpeakWriteGo",
-    py_modules=["whisper"],
     version='0.1',
     description="A Library for transcribing audio to text",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    readme="README.md",
-    python_requires=">=3.7",
     author="Emmaricia",
     url="https://github.com/openai/whisper",
     license="MIT",
