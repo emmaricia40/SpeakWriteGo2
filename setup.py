e881063ae8c64f69ca398a1248aa99459993b196
emmaricia40 <124712414+emmaricia40@users.noreply.github.com>
2023-02-10 16:23:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     py_modules=["whisper"],
     version=0.1(),
     description="A library for transcribing audio to text",
-    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     readme="README.md",
     python_requires=">=3.7",
