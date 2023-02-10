9787c6f2eb14b007cc089e908d4b9f3d06d55732
emmaricia40 <124712414+emmaricia40@users.noreply.github.com>
2023-02-10 16:18:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='SpeakWriteGo',
-    version='0.1',
-    description='A library for transcribing audio to text',
-    author='Emmaricia Madison',
-    author_email='emmaricia@gmail.com',
-    url='https://github.com/emmaricia40/SpeakWriteGo',
-    packages=['SpeakWriteGo'],
+    name="SpeakWriteGo",
+    py_modules=["whisper"],
+    version=read_version(),
+    description="A library for transcribing audio to text",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    python_requires=">=3.7",
+    author="Emmaricia",
+    url="https://github.com/emmaricia40/SpeakWriteGo.git",
+    license="MIT",
+    packages=find_packages(exclude=["tests*"]),
     install_requires=[
-        'numpy',
-        'pytorch',
-        'openai',
-        'openai-whisper',
-        'streamlit'
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
     ],
+    entry_points={
+        "console_scripts": ["whisper=whisper.transcribe:cli"],
+    },
+    include_package_data=True,
+    extras_require={"dev": ["pytest"]},
 )
